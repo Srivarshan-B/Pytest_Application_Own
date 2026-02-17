417f204f59c2364df31ba5c7e228615f71b65464
SreeVars <srivarshan979@gmail.com>
2026-02-17 11:37:46 +0530

    Add the concept of Parameterized Fixtures

--- pytest_Learning/conftest.py
+++ pytest_Learning/conftest.py
@@ -1,4 +1,10 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
@@ -6,3 +12,18 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     setattr(item, "rep_" + rep.when, rep)
     return rep
+
+
+@pytest.fixture(params=["chrome", "firefox"], scope="function")
+def get_browser(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    if request.param == "firefox":
+        driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get("https://www.w3schools.com/")
+    print("Open the browser")
+    yield driver
+    driver.quit()
+    print("Close the browser")
--- pytest_Learning/webdriver_Integration.py
+++ pytest_Learning/webdriver_Integration.py
@@ -6,8 +6,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import xpaths
-
-
+# from pytest_Learning.conftest import get_browser
 
 
 def get_the_data_from_the_user():
@@ -16,37 +15,41 @@ def get_the_data_from_the_user():
         ("dani@gmail.com", "ksfmlamf"),
         ("sam@gmail.com", "fjasofnwoo"),
         ("roh@gmail.com", "jfnlafnl"),
-        ]
+    ]
+
 
 @pytest.fixture()
-def logon_failure(request):
+def logon_failure(request, get_browser):
     yield
     item = request.node
     if item.rep_call.failed:
-         allure.attach(driver.get_screenshot_as_png(), name="login_ss", attachment_type=AttachmentType.PNG)
+        allure.attach(driver.get_screenshot_as_png(), name="login_ss", attachment_type=AttachmentType.PNG)
 
-def setup_function(function):
-    global driver
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    driver.get("https://www.w3schools.com/")
-    print("Open the browser")
 
-def teardown_function(function):
-    driver.quit()
-    print("Closed the browser")
+# def setup_function(function):
+#     global driver
+#     service = Service(ChromeDriverManager().install())
+#     driver = webdriver.Chrome(service=service)
+#     driver.maximize_window()
+#     driver.implicitly_wait(10)
+#     driver.get("https://www.w3schools.com/")
+#     print("Open the browser")
+#
+#
+# def teardown_function(function):
+#     driver.quit()
+#     print("Closed the browser")
+
 
 @pytest.mark.usefixtures("logon_failure")
 @pytest.mark.parametrize("username, password", get_the_data_from_the_user())
-def test_login(username, password):
+def test_login(username, password, get_browser):
+    driver = get_browser
     driver.find_element(By.XPATH, xpaths.sign_in).click()
     driver.find_element(By.XPATH, xpaths.emmail).send_keys(username)
     driver.find_element(By.XPATH, xpaths.pwd).send_keys(password)
     driver.find_element(By.XPATH, xpaths.visible).click()
-    assert 1 == 2
+    # assert 1 == 2
     # allure.attach(driver.get_screenshot_as_png(), name="login_ss", attachment_type=AttachmentType.PNG)
     # driver.find_element(By.XPATH, xpaths.button).click()
     print("Completed the login successfully")
-
